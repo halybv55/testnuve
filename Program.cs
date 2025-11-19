2e163c98e5376c6f7fc264f7b426d92f6073f3d5
halybv55 <merunidase@gmail.com>
2025-11-19 09:39:16 -0400

    Configure web host to use custom URL

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.WebHost.UseUrls("http://0.0.0.0:8080");
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -23,3 +24,4 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
